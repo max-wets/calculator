2b8e2e986112c32c79a21d0e0f360903db778c7b
Maxime Wets <wetsmaxime@gmail.com>
2023-11-23 20:11:39 +0100

    Add sum logic, controller and unit test

---
+++ src/main/java/com/wets/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package com.wets.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
